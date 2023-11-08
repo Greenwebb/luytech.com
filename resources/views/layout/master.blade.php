aed546c8b5af4cfc3eadf5dc42895b38965da627
unknown <theegreenweb@gmail.com>
2023-11-08 13:35:43 +0200

    Beta stage

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <title>@yield('title','') | Neptune - Multipurpose Bootstrap Admin Template </title>
+    <title>Administration | Luytech Motors </title>
     <!-- initiate head with meta tags, css and script -->
     @include('include.head')
 
